3765ecba2d81faafccc5b0206245c501d64ed50c
Abdur Rahman <abdurrahman489@gmail.com>
2025-03-20 14:36:04 +0530

    added the initial files and dependencies

---
+++ index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5012;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const connectDB = async () => {
+  mongoose.connect(process.env.MONGO_SERVER_URI);
+};
+
+connectDB()
+  .then(() => {
+    console.log("connection with MongoDB established");
+  })
+  .catch((error) => {
+    console.error("connection with MongoDB was not established ", error);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
